6baf4e785e7800f9f689baa37a2e14a566a2e8bb
Clueless Wizard <108606804+cluelesswizard@users.noreply.github.com>
2022-07-08 13:06:00 +0700

    Create task01.py

---
+++ task01.py
@@ -0,0 +1,38 @@
+# import library python
+from os import system, name
+
+# kode warna
+rt = '\033[0m'
+yl = '\033[93m'
+lc = '\033[96m'
+gr = '\033[32m'
+pu = '\033[35m'
+
+# fungsi clear terminal
+def clear():
+    # jika os windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # jika os mac / linux
+    else:
+        _ = system('clear')
+
+# trigger clear terminal
+clear()
+
+# dekorasi
+print("\n\t{}------------------------{} Input Data {}-------------------------" .format(pu,rt,pu))
+
+# meminta input dari user
+nama = input("\n\t{}[{}?{}] Masukkan nama anda    : {}" .format(rt,yl,rt,lc))
+kelas = input("\t{}[{}?{}] Masukkan kelas anda   : {}" .format(rt,yl,rt,lc))
+jurusan = input("\t{}[{}?{}] Masukkan jurusan anda : {}" .format(rt,yl,rt,lc))
+
+# dekorasi
+print("\n\t{}-----------------------{} Output Data {}-------------------------" .format(pu,rt,pu))
+
+# menampilkan output
+print("\n\t{}[{}√{}] Nama anda    : {}" .format(rt,gr,rt,yl) + nama)
+print("\t{}[{}√{}] Kelas anda   : {}" .format(rt,gr,rt,yl) + kelas)
+print("\t{}[{}√{}] Jurusan anda : {}" .format(rt,gr,rt,yl) + jurusan + rt)
